119dcec6dd6e8673ffc7035dc0c55d79651b97fc
Camden Kay <ckay@orioles.com>
2022-12-01 10:37:12 -0500

    Day one!

---
+++ 2022/day_01.R
@@ -0,0 +1,41 @@
+library(tidyverse)
+library(rvest)
+source("data_helper.R")
+
+
+raw_data <- get_data(1, 
+                     keyring::key_get("RStudio Keyring Secrets", 
+                                      "Advent of Code Session Cookie"),
+                     2022) %>% 
+  httr::content(encoding = "UTF-8") %>% 
+  read_lines()
+
+
+data <- tibble(calorie_count = raw_data) %>% 
+  mutate(calorie_count = as.numeric(calorie_count))
+
+
+elf_calorie_counts <- data %>% 
+  mutate(new_elf = if_else(row_number() == 1, 
+                           TRUE, 
+                           is.na(lag(calorie_count)))) %>% 
+  filter(!is.na(calorie_count)) %>% 
+  mutate(elf_id = cumsum(new_elf)) %>% 
+  group_by(elf_id) %>% 
+  summarize(n_snacks = n(),
+            total_calories = sum(calorie_count),
+            .groups = "drop") %>% 
+  arrange(-total_calories)
+
+
+
+# Part 1
+elf_calorie_counts %>% 
+  pull(total_calories) %>% 
+  max()
+
+# Part 2
+elf_calorie_counts %>% 
+  slice_max(total_calories, n = 3) %>% 
+  pull(total_calories) %>% 
+  sum()
