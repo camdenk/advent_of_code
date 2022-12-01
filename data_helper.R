a84d940cfa7876eb4b99119b8c869cc38a0fb9d2
Camden Kay <ckay@orioles.com>
2022-12-01 10:59:43 -0500

    Update get data function

--- data_helper.R
+++ data_helper.R
@@ -1,5 +1,7 @@
 
-.get_data <- function(day, session_cookie, year) {
+.get_data <- function(day, year,
+                      session_cookie = keyring::key_get("RStudio Keyring Secrets", 
+                                                        "Advent of Code Session Cookie")) {
   aoc_url <- glue::glue("adventofcode.com/{year}/day/{day}/input")
   cookie <- httr::set_cookies(session = session_cookie)
   response <- httr::GET(aoc_url, cookie)
